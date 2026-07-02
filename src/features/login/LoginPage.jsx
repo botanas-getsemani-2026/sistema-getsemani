cec6d0df456bbf994fb4e844eef987e7d8af94c2
Enrique Sánchez <dilanenrique555@gmail.com>
2026-07-02 16:19:03 -0600

    feat: add login feature

---
+++ src/features/login/LoginPage.jsx
@@ -0,0 +1,9 @@
+import LoginForm from './components/LoginForm'
+
+export default function LoginPage() {
+	return (
+    <main className='w-screen h-screen bg-background flex items-center justify-center'>
+      <LoginForm />
+    </main>
+  )
+}
---
+++ src/features/login/components/LoginForm.jsx
@@ -0,0 +1,49 @@
+import { EyeClosed } from 'lucide-react'
+import { Eye } from 'lucide-react'
+import { useState } from 'react'
+
+export default function LoginForm() {
+	const [showPassword, setShowPassword] = useState(false)
+
+	const handleShowPassword = () => {
+		setShowPassword(!showPassword)
+	}
+
+	return (
+		<form
+			action='submit'
+			className='bg-on-primary-container flex flex-col justify-center items-center gap-8 w-1/3 h-1/2 p-8 border rounded-md'
+		>
+			<img src='src/assets/getsemani-logo-v3-name.webp' alt='Getsemani Logo' />
+			<input
+				className='w-full p-2 border border-on-primary rounded-sm text-primary-container text-lg focus:outline-none focus:ring-primary-container focus:ring-1'
+				name='username'
+				type='text'
+				placeholder='usuario@getsemani.com'
+				required
+			/>
+			<div className='flex justify-center items-center w-full border border-on-primary rounded-sm text-primary-container text-lg'>
+				<input
+					className='w-full p-2 focus:outline-none'
+					name='password'
+					type={showPassword ? 'text' : 'password'}
+					placeholder='contraseña'
+					required
+				/>
+				<button
+					className='p-2 text-lg text-primary-container hover:text-primary-container/80'
+					type='button'
+					onClick={handleShowPassword}
+				>
+					{showPassword ? <Eye /> : <EyeClosed />}
+				</button>
+			</div>
+			<button
+				className='border border-on-primary rounded-md bg-primary-container w-1/2 text-on-primary-container h-10 text-lg font-semibold cursor-pointer hover:bg-primary-container/90 transition-colors'
+				type='submit'
+			>
+				Iniciar sesión
+			</button>
+		</form>
+	)
+}
