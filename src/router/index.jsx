cb7259a22a3a29c20d9397fed364a1be56a5de25
Enrique Sánchez <dilanenrique555@gmail.com>
2026-08-10 19:46:53 -0600

    refactor: change de components names and files names

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -7,8 +7,10 @@ import { StockLoadPage } from '../features/stockLoad/StockLoadPage'
 import { VendorsPage } from '../features/vendors/VendorsPage'
 import { ProtectedRoute } from './ProtectedRoute'
 import { PublicOnlyRoute } from './PublicOnlyRoute'
-import { ComisionesPage } from '../features/comisiones/ComisionesPage'
-import { PeriodosPage } from '../features/comisiones/PeriodosPage'
+// import { ComisionesPage } from '../features/comisiones/ComisionesPage'
+// import { PeriodosPage } from '../features/comisiones/PeriodosPage'
+import { CommissionsPage } from '../features/comisiones/CommissionsPage'
+import { PeriodsPage } from '../features/comisiones/PeriodsPage'
 
 export const router = createBrowserRouter([
   {
@@ -30,8 +32,8 @@ export const router = createBrowserRouter([
 					{ path: 'cargas', element: <StockLoadPage /> },
 					{ path: 'productos', element: <ProductsPage /> },
 					{ path: 'vendedores', element: <VendorsPage /> },
-					{ path: 'comisiones', element: <ComisionesPage /> },
-					{ path: 'comisiones/periodos', element: <PeriodosPage /> },
+					{ path: 'comisiones', element: <CommissionsPage /> },
+					{ path: 'comisiones/periodos', element: <PeriodsPage /> },
 				],
 			},
 		],
